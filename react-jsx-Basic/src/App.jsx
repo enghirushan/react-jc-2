92fbf86bdb1cefc16117ae947f4014c476207479
enghirushan <eng.hirushan@gmail.com>
2024-12-24 20:34:59 +0530

    add

--- react-jsx-Basic/src/App.jsx
+++ react-jsx-Basic/src/App.jsx
@@ -1,20 +1,27 @@
 
 function App() {
   
+  const firstName="Haritha";
+  const MiddleName = "Hirushan";
+  const  LastName = "ilangasinghe";
+
   const getFullName = (f,m,s) =>{ /*arrow function*/ 
       return `${f} ${m} ${s}`; /* Template literals */
 
   }
-
-
+  
+  const arr = ["apple" , "Graph" , "Oranges"];
+   
   return (
     <>
       <div className="cout">
       <h1>Employee Details</h1>
       <p>Full Name function :{getFullName(" Haritha" ,"Hirushan","ilangasinghe")}</p>
+
+      <p>{firstName} likes to eat {arr[0]}</p>
       </div>
 
-
+        <p>hhh</p>
     </>
   )
 }
